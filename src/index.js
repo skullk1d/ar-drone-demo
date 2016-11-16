601b497f34a1fc99d450e648ed57cd6e120516e9
Tarick <gth766q@gatech.edu>
2016-11-16 23:56:47 +0900

    run missions with repl using only ar-drone base library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // entry point
 var arDrone = require('ar-drone');
-var autonomy = require('ardrone-autonomy');
 
 // import mission(s) we wish to run
 // cycle this value to choose mission from missions (here via source or in config)
@@ -11,11 +10,22 @@ var ACTIVE_MISSION = 'WaterSensor'; // point to mission file in missions folder
 var ActiveMission = require('./missions/' + ACTIVE_MISSION);
 var activeMission = new ActiveMission();
 
+// listen for entered data on command line
+var stdin = process.openStdin();
+
+stdin.addListener('data', function(d) {
+	// NOTE: d is an object, and when converted to a string it will end with a linefeed
+	// on any input, end mission and hover -- await repl commands
+	activeMission.endMission();
+});
+
 // begin programmed sequence
 activeMission.startMission();
 
 // repl (active command line while mission is running)
 // use to land or steer drone immediately, which overrides running mission
+// context scope is "client"
+// (simple run any client command "takeoff()" "land()" etc)
 // NOTE: ar-drone logs client to console on takeoff every time
 // NOTE: repl remains open on mission override
-activeMission.mission.startRepl();
+activeMission.client.createRepl();
--- src/missions/WaterSensor.js
+++ src/missions/WaterSensor.js
@@ -1,7 +1,5 @@
 var ardrone = require('ar-drone');
-var autonomy = require('ardrone-autonomy');
 var EventEmitter = require('events').EventEmitter;
-var ReplMission = require('../ReplMission');
 var util = require('util');
 
 function WaterSensor(clientOptions) {
@@ -10,60 +8,30 @@ function WaterSensor(clientOptions) {
 	this.name = 'Water Sensor';
 
 	// init mission
-	var client  = ardrone.createClient(clientOptions);
-	var control = new autonomy.Controller(client, clientOptions);
-	this.mission = new ReplMission(client, control, clientOptions);
-
-	// TEST: simple hover delay to check repl abort functionality
-	this.mission.takeoff()
-		.wait(4000)
-		.land();
-
-	// define mission actions
-	/*this.mission.takeoff()
-		.zero()		// sets current state as reference / current position as origin
-		.altitude(1)  // climb altitude (meters)
-		.forward(2)
-		.altitude(0.1)
-		.hover(3000)  // hover
-		.altitude(1)
-		.cw(180)
-		.forward(2)
-		.cw(180)
-		.land()*/
+	this.client = ardrone.createClient(clientOptions);
 }
 
 util.inherits(WaterSensor, EventEmitter);
 
 WaterSensor.prototype.startMission = function () {
-	var self = this;
-
 	console.log('Starting mission ' + this.name);
 
-	var mission = this.mission;
+	var client = this.client;
 
-	// run with error handling
-	mission.run(function (err, result) {
-		if (err) {
-			self.endMission();
+	// mission sequence here
+	client.ftrim();
+	client.takeoff();
+ 	client.after(8000, function() {
+ 		this.stop(); // hover in auto-pilot
+ 		this.land();
+ 	});
 
-			console.log("Oops, something bad happened: %s", err.message);
-
-			self.emit('fail');
-		} else {
-			console.log("Mission success!");
-
-			self.emit('success');
-
-			process.exit(0); // exit node
-		}
-	});
+ 	// process.exit(0); // exit node (remove if want to keep node running after mission)
 };
 
  WaterSensor.prototype.endMission = function () {
 	// end mission immediately
-	this.mission.client().stop();
-	this.mission.client().land();
+	this.client.stop();
 
 	this.emit('end');
 };
