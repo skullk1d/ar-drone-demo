b19a92c9a5f0f10f1ab0b2042580c157cc36bc64
Tarick <gth766q@gatech.edu>
2016-11-26 12:38:21 +0900

    updated water sensor mission

--- src/missions/WaterSensor.js
+++ src/missions/WaterSensor.js
@@ -19,15 +19,38 @@ WaterSensor.prototype.startMission = function () {
 	var client = this.client;
 
 	// mission sequence here
+	var SPEED = 0.05;
+
+	var tTakeoff = 5000;
+	var tRotate = (180 / Math.PI / SPEED); // v = d/t = rads/t => t = rads/v
+
 	client.ftrim();
 	client.takeoff();
- 	client.after(6000, function() {
- 		this.up(0.5);
- 	});
- 	client.after(2000, function() {
- 		this.stop(); // hover in auto-pilot
- 		this.land();
- 	});
+	client.after(tTakeoff, function() { // ascend
+		this.up(SPEED);
+	}).after(500, function() { // hover then approach water
+		this.stop();
+		this.forward(SPEED);
+	}).after(1000, function() { // hover then descend
+		this.stop();
+		this.down(SPEED);
+	}).after(500, function() { // hover and (activate sensor over water?)
+		this.stop();
+	}).after(2000, function() { // after 2 sec, ascend
+		this.up(SPEED);
+	}).after(500, function() { // turn around 180 deg
+		this.stop();
+		this.clockwise(SPEED);
+	}).after(tRotate, function() { // hover and go forward to starting point
+		this.stop();
+		this.forward(SPEED);
+	}).after(1000, function() { // turn around 180 deg
+		this.stop();
+		this.clockwise(SPEED);
+	}).after(tRotate, function() { // hover and land
+		this.stop();
+		this.land();
+	});
 };
 
  WaterSensor.prototype.endMission = function () {
